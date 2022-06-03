21c50d929f994619f8a98f7e22b55172f70317a0
theMelonMusk <qmindzoom@gmail.com>
2022-06-03 12:06:41 +0530

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+Container roundedSquare({
+  double width = 150,
+  double height = 150,
+  required Color bgColor,
+  required Color textColor,
+  required String text,
+}) {
+  return Container(
+    child: Center(
+      child: Text(
+        text,
+        style: TextStyle(fontWeight: FontWeight.bold, color: textColor),
+      ),
+    ),
+    width: width,
+    margin: EdgeInsets.symmetric(horizontal: 10.0),
+    height: height,
+    decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(15.0),
+        border: Border.all(color: Colors.blue, width: 1)),
+  );
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            //mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(width: double.infinity),
+              SizedBox(
+                height: 10,
+              ),
+              roundedSquare(
+                  textColor: Colors.black,
+                  bgColor: Colors.yellow,
+                  text: "Square1"),
+              SizedBox(
+                height: 10,
+              ),
+              roundedSquare(
+                text: "Square2",
+                bgColor: Colors.orange,
+                textColor: Colors.blue,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              roundedSquare(
+                  bgColor: Colors.green,
+                  textColor: Colors.yellow,
+                  text: "Square3"),
+              SizedBox(
+                height: 10,
+              ),
+              roundedSquare(
+                  width: 200,
+                  height: 200,
+                  textColor: Colors.black,
+                  bgColor: Colors.yellow,
+                  text: "Square1"),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
